98d3f3849555632c8a24421b63e3548c7db01df0
SHRIYANS ROY <83387141+roys338@users.noreply.github.com>
2021-06-27 22:05:12 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Hollywood Specials</h1>
+      <p>Best Songs of the Year from Favourite Artists</p>
+    </div>
+  );
+}
+
+export default Info;
